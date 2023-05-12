d55e52f95245418019a29241f6c792c0301dc2ea
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2023-05-11 21:28:12 -0600

    Cambios y configuración de Seguridad
    Agregue el codigo vw_Seguridad para que se ha más fácil la programación (Auto Complete)

--- Proyecto/Interfaces/seguridad_Window.py
+++ Proyecto/Interfaces/seguridad_Window.py
@@ -3,52 +3,51 @@
 import sys
 from datetime import datetime
 
-from PyQt5 import QtWidgets
 from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
-from PyQt5.uic import loadUi
 
 from Datos import dt_Usuario, dt_Rol, dt_Opcion
 from Entidades.Usuarios import Usuarios
 from Entidades.Roles import Rol
 from Entidades.Opciones import Opcion
 
-# Indicador obtiende de las carpeta de las clases de Datos, un valor verdadero o falso para asi poder mostrar un mensaje de confirmación ya se ha guardar, editar o eliminar datos
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from Interfaces import vw_Seguridad
 
 
-class frm_Seguridad(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(frm_Seguridad,self).__init__()
-        loadUi("./Seguridad.ui",self)
+class seguridad_Window(QMainWindow, vw_Seguridad.Ui_Seguridad):
+    def __init__(self, parent=None):
+        super(seguridad_Window, self).__init__(parent)
+        self.setupUi(self)
 
-        #Acciones de botones
+        # Acciones de botones
 
-        #Botones de Usuario
+        # Botones de Usuario
         self.bt_Guardar_Usuario.clicked.connect(self.guardarUsuario)
         self.bt_Editar_Usuario.clicked.connect(self.editarUsuario)
         self.bt_Eliminar_Usuario.clicked.connect(self.eliminarUsuario)
         self.bt_Vaciar_Usuario.clicked.connect(self.limpiarCampos)
 
-        #Botones de Rol
+        # Botones de Rol
         self.bt_Guardar_Rol.clicked.connect(self.guardarRol)
         self.bt_Editar_Rol.clicked.connect(self.editarRol)
         self.bt_Eliminar_Rol.clicked.connect(self.eliminarRol)
         self.bt_Vaciar_Rol.clicked.connect(self.limpiarCampos)
 
-        #Botones de Opcion
+        # Botones de Opcion
         self.bt_Guardar_Opcion.clicked.connect(self.guardarOpcion)
         self.bt_Editar_Opcion.clicked.connect(self.editarOpcion)
         self.bt_Eliminar_Opcion.clicked.connect(self.eliminarOpcion)
         self.bt_Vaciar_Opcion.clicked.connect(self.limpiarCampos)
 
+        # Acciones de las tablas
 
-        #Acciones de las tablas
-
-        #Usuarios
+        # Usuarios
         self.llenarTablaUsuario(dt_Usuario.Dt_Usuarios.listarUsuarios())
         self.tb_Usuario.itemSelectionChanged.connect(self.obtenerDatosTablaUsuario)
 
-        #Roles
+        # Roles
         self.llenarTablaRol(dt_Rol.Dt_Rol.listarRol())
         self.tb_Rol.itemSelectionChanged.connect(self.obtenerDatosTablaRol)
 
@@ -56,55 +55,49 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         self.llenarTablaOpcion(dt_Opcion.Dt_Opcion.listarOpcion())
         self.tb_Opcion.itemSelectionChanged.connect(self.obtenerDatosTablaOpcion)
 
-
     '''***********************************************  Funciones reutilizables   ******************************************'''
-
 
     def limpiarCampos(self):
 
-        if not self.line_Usuario_Nombre.text() == "" or not self.line_Usuario_Apellido.text() == "" or not self.line_Usuario_User.text() == "" or not self.line_Usuario_Password.text() == "" :
-            print("Datos limpiados de la ventana Usuario")
+        if not self.line_Usuario_Nombre.text() == "" or not self.line_Usuario_Apellido.text() == "" or not self.line_Usuario_User.text() == "" or not self.line_Usuario_Password.text() == "":
+            print("\n Datos limpiados de la ventana Usuario")
             self.line_Usuario_Id.clear()
             self.line_Usuario_Nombre.clear()
             self.line_Usuario_Apellido.clear()
             self.line_Usuario_User.clear()
             self.line_Usuario_Password.clear()
 
-        #Rol
+        # Rol
         if not self.line_Rol_Id.text() == "" or not self.line_Rol.text() == "":
-            print("Datos limpiados de la ventana Rol")
+            print("\nDatos limpiados de la ventana Rol")
             self.line_Rol_Id.clear()
             self.line_Rol.clear()
 
-        #Opcion
+        # Opcion
         if not self.line_Opcion_Id.text() == "" or not self.line_Opcion.text() == "":
-            print("Datos limpiados de la ventana Opcion")
+            print("\nDatos limpiados de la ventana Opcion")
             self.line_Opcion_Id.clear()
             self.line_Opcion.clear()
 
-
-
-
     def notifMensaje(self, indicador, resultado):
 
-        if indicador == True: #Se hizo correctamente la consulta a la base de datos
+        if indicador == True:  # Se hizo correctamente la consulta a la base de datos
             QMessageBox.about(self, "Exito!", "Los Datos Fueron " + resultado + " Correctamente")
 
-        else:#No se hizo correctamente la consulta a la base de datos
+        else:  # No se hizo correctamente la consulta a la base de datos
             QMessageBox.about(self, "Error", "Ha Ocurrido un Error")
-
 
         '''***********************************************  Usuario   ******************************************'''
 
     def guardarUsuario(self):
 
         try:
-            #Transformar fecha en formato "yyyy-MM-dd" para hacer la consulta al sql
+            # Transformar fecha en formato "yyyy-MM-dd" para hacer la consulta al sql
             fecha = self.line_Usuario_Fecha.text()
             fecha_objeto = datetime.strptime(fecha, "%d/%m/%Y")
             fechaTransformada = fecha_objeto.strftime("%Y-%m-%d")
 
-            #Programación orientada a objetos
+            # Programación orientada a objetos
 
             Usuarios.nombre = self.line_Usuario_Nombre.text()
             Usuarios.apellido = self.line_Usuario_Apellido.text()
@@ -112,15 +105,17 @@ class frm_Seguridad(QtWidgets.QMainWindow):
             Usuarios.password = self.line_Usuario_Password.text()
             Usuarios.fechaCreacion = fechaTransformada
 
-            if self.line_Usuario_Id.text()== "" and not self.line_Usuario_Nombre.text()== "" and not self.line_Usuario_Apellido.text()== "" and not self.line_Usuario_User.text()== "" and not self.line_Usuario_Password.text()== "" and not self.line_Usuario_Fecha.text()== "":
+            if self.line_Usuario_Id.text() == "" and not self.line_Usuario_Nombre.text() == "" and not self.line_Usuario_Apellido.text() == "" and not self.line_Usuario_User.text() == "" and not self.line_Usuario_Password.text() == "" and not self.line_Usuario_Fecha.text() == "":
 
-                indicador = dt_Usuario.Dt_Usuarios.guardarUsuario(Usuarios)  # Recoge los datos en los "Lines" de Qt Desinger para editarlos en la base de datos
+                indicador = dt_Usuario.Dt_Usuarios.guardarUsuario(
+                    Usuarios)  # Recoge los datos en los "Lines" de Qt Desinger para editarlos en la base de datos
 
-                self.notifMensaje(indicador,"Guardados")
+                self.notifMensaje(indicador, "Guardados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaUsuario(dt_Usuario.Dt_Usuarios.listarUsuarios())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaUsuario(
+                    dt_Usuario.Dt_Usuarios.listarUsuarios())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -130,13 +125,11 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         except Exception as e:
             print(f"Error: {e}")
 
-
-
     def editarUsuario(self):
 
         try:
 
-            #Transformar fecha en formato "yyyy-MM-dd" para hacer la consulta al sql
+            # Transformar fecha en formato "yyyy-MM-dd" para hacer la consulta al sql
             fecha = self.line_Usuario_Fecha.text()
             fecha_objeto = datetime.strptime(fecha, "%d/%m/%Y")
             fechaTransformada = fecha_objeto.strftime("%Y-%m-%d")
@@ -148,15 +141,17 @@ class frm_Seguridad(QtWidgets.QMainWindow):
             Usuarios.password = self.line_Usuario_Password.text()
             Usuarios.fechaCreacion = fechaTransformada
 
-            if not self.line_Usuario_Id.text()== "" and not self.line_Usuario_Nombre.text()== "" and not self.line_Usuario_Apellido.text()== "" and not self.line_Usuario_User.text()== "" and not self.line_Usuario_Password.text()== "" and not self.line_Usuario_Fecha.text()== "":
+            if not self.line_Usuario_Id.text() == "" and not self.line_Usuario_Nombre.text() == "" and not self.line_Usuario_Apellido.text() == "" and not self.line_Usuario_User.text() == "" and not self.line_Usuario_Password.text() == "" and not self.line_Usuario_Fecha.text() == "":
 
-                indicador = dt_Usuario.Dt_Usuarios.editarUsuario(Usuarios)  # Recoge los datos en los "Lines" de Qt Desinger para editarlos en la base de datos
+                indicador = dt_Usuario.Dt_Usuarios.editarUsuario(
+                    Usuarios)  # Recoge los datos en los "Lines" de Qt Desinger para editarlos en la base de datos
 
-                self.notifMensaje(indicador,"Editados")
+                self.notifMensaje(indicador, "Editados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaUsuario(dt_Usuario.Dt_Usuarios.listarUsuarios())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaUsuario(
+                    dt_Usuario.Dt_Usuarios.listarUsuarios())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -165,8 +160,6 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
         except Exception as e:
             print(f"Error: {e}")
-
-
 
     def eliminarUsuario(self):
 
@@ -174,15 +167,16 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
             Usuarios.id_usuario = self.line_Usuario_Id.text()
 
-            if not self.line_Usuario_Id.text()== "" and not self.line_Usuario_Nombre.text()== "" and not self.line_Usuario_Apellido.text()== "" and not self.line_Usuario_User.text()== "" and not self.line_Usuario_Password.text()== "" and not self.line_Usuario_Fecha.text()== "":
+            if not self.line_Usuario_Id.text() == "" and not self.line_Usuario_Nombre.text() == "" and not self.line_Usuario_Apellido.text() == "" and not self.line_Usuario_User.text() == "" and not self.line_Usuario_Password.text() == "" and not self.line_Usuario_Fecha.text() == "":
 
                 indicador = dt_Usuario.Dt_Usuarios.eliminarUsuario(Usuarios)
 
-                self.notifMensaje(indicador,"Eliminados")
+                self.notifMensaje(indicador, "Eliminados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaUsuario(dt_Usuario.Dt_Usuarios.listarUsuarios())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaUsuario(
+                    dt_Usuario.Dt_Usuarios.listarUsuarios())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -192,11 +186,9 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         except Exception as e:
             print(f"Error: {e}")
 
-
-
     def obtenerDatosTablaUsuario(self):
 
-        #Selecciona la fila de la tabla
+        # Selecciona la fila de la tabla
         filaSeleccionada = self.tb_Usuario.currentRow()
         id = self.tb_Usuario.item(filaSeleccionada, 0).text()
         nombre = self.tb_Usuario.item(filaSeleccionada, 1).text()
@@ -205,10 +197,10 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         clave = self.tb_Usuario.item(filaSeleccionada, 4).text()
         fecha = self.tb_Usuario.item(filaSeleccionada, 5).text()
 
-        #Tramsformar la fecha en formato "yyyy-MM-dd"
+        # Tramsformar la fecha en formato "yyyy-MM-dd"
         fechaTransformada = QDate.fromString(fecha, "yyyy-MM-dd")
 
-        #Asigna el contenido de la tabla seleccionada a los Edits Lines Correspondientes
+        # Asigna el contenido de la tabla seleccionada a los Edits Lines Correspondientes
         self.line_Usuario_Id.setText(id)
         self.line_Usuario_Nombre.setText(nombre)
         self.line_Usuario_Apellido.setText(apellido)
@@ -216,11 +208,9 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         self.line_Usuario_Password.setText(clave)
         self.line_Usuario_Fecha.setDate(fechaTransformada)
 
-
-
     def llenarTablaUsuario(self, datos):
 
-        print("Datos de la Tablas Usuarios")
+        print("\nDatos de la Tablas Usuarios")
         i = len(datos)
         self.tb_Usuario.setRowCount(i)
         tablerow = 0
@@ -236,10 +226,7 @@ class frm_Seguridad(QtWidgets.QMainWindow):
             self.tb_Usuario.setItem(tablerow, 6, QTableWidgetItem(str(row["estado"])))
             tablerow = tablerow + 1
 
-
-
         '''******************************************  Rol   ******************************************'''
-
 
     def guardarRol(self):
 
@@ -251,11 +238,12 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
                 indicador = dt_Rol.Dt_Rol.guardarRol(Rol)
 
-                self.notifMensaje(indicador,"Guardados")
+                self.notifMensaje(indicador, "Guardados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaRol(dt_Rol.Dt_Rol.listarRol())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaRol(
+                    dt_Rol.Dt_Rol.listarRol())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -263,8 +251,6 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
         except Exception as e:
             print(f"Error: {e}")
-
-
 
     def editarRol(self):
 
@@ -276,11 +262,12 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
                 indicador = dt_Rol.Dt_Rol.editarRol(Rol)
 
-                self.notifMensaje(indicador,"Editados")
+                self.notifMensaje(indicador, "Editados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaRol(dt_Rol.Dt_Rol.listarRol())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaRol(
+                    dt_Rol.Dt_Rol.listarRol())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -289,8 +276,6 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
         except Exception as e:
             print(f"Error: {e}")
-
-
 
     def eliminarRol(self):
 
@@ -301,11 +286,12 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
                 indicador = dt_Rol.Dt_Rol.eliminarRol(Rol)
 
-                self.notifMensaje(indicador,"Eliminados")
+                self.notifMensaje(indicador, "Eliminados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaRol(dt_Rol.Dt_Rol.listarRol())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaRol(
+                    dt_Rol.Dt_Rol.listarRol())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -315,24 +301,20 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         except Exception as e:
             print(f"Error: {e}")
 
-
-
     def obtenerDatosTablaRol(self):
 
-        #Selecciona la fila de la tabla
+        # Selecciona la fila de la tabla
         filaSeleccionada = self.tb_Rol.currentRow()
         id = self.tb_Rol.item(filaSeleccionada, 0).text()
         rol = self.tb_Rol.item(filaSeleccionada, 1).text()
 
-        #Asigna el contenido de la tabla seleccionada a los Edits Lines Correspondientes
+        # Asigna el contenido de la tabla seleccionada a los Edits Lines Correspondientes
         self.line_Rol_Id.setText(id)
         self.line_Rol.setText(rol)
 
-
-
     def llenarTablaRol(self, datos):
 
-        print("Datos de la Tabla rol")
+        print("\nDatos de la Tabla rol")
         i = len(datos)
         self.tb_Rol.setRowCount(i)
         tablerow = 0
@@ -359,7 +341,8 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
                 self.limpiarCampos()
 
-                self.llenarTablaOpcion(dt_Opcion.Dt_Opcion.listarOpcion())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaOpcion(
+                    dt_Opcion.Dt_Opcion.listarOpcion())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -367,8 +350,6 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
         except Exception as e:
             print(f"Error: {e}")
-
-
 
     def editarOpcion(self):
 
@@ -384,7 +365,8 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
                 self.limpiarCampos()
 
-                self.llenarTablaOpcion(dt_Opcion.Dt_Opcion.listarOpcion())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaOpcion(
+                    dt_Opcion.Dt_Opcion.listarOpcion())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -393,8 +375,6 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
         except Exception as e:
             print(f"Error: {e}")
-
-
 
     def eliminarOpcion(self):
 
@@ -409,7 +389,8 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
                 self.limpiarCampos()
 
-                self.llenarTablaOpcion(dt_Opcion.Dt_Opcion.listarOpcion())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaOpcion(
+                    dt_Opcion.Dt_Opcion.listarOpcion())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -418,8 +399,6 @@ class frm_Seguridad(QtWidgets.QMainWindow):
 
         except Exception as e:
             print(f"Error: {e}")
-
-
 
     def obtenerDatosTablaOpcion(self):
 
@@ -432,10 +411,9 @@ class frm_Seguridad(QtWidgets.QMainWindow):
         self.line_Opcion_Id.setText(id)
         self.line_Opcion.setText(opcion)
 
-
     def llenarTablaOpcion(self, datos):
 
-        print("Datos de la Tabla opcion")
+        print("\nDatos de la Tabla opcion")
         i = len(datos)
         self.tb_Opcion.setRowCount(i)
         tablerow = 0
@@ -446,14 +424,11 @@ class frm_Seguridad(QtWidgets.QMainWindow):
             self.tb_Opcion.setItem(tablerow, 1, QTableWidgetItem((row["descripcion"])))
             tablerow = tablerow + 1
 
-
-
         '''******************************************  Menu Principal   ******************************************'''
 
 
-
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    aplicacion = frm_Seguridad()
-    aplicacion.show()
+    app = QApplication(sys.argv)
+    mw = seguridad_Window()
+    mw.show()
     app.exec()
