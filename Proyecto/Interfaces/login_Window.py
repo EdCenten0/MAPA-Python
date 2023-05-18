dfd8c6d059c57bf8953172500e1defc59f0d3957
Francisco_Melendez <francisco.melendez14454@est.uca.edu.ni>
2023-05-18 11:32:54 -0600

    Agregue el Ui y la ventana de Login y registrar usuario, al igual agregue el logo de MAPA en recursos
    Contiene un pequeño bug a la hora de abrir las ventanas del login y registrar 2 veces

--- Proyecto/Interfaces/login_Window.py
+++ Proyecto/Interfaces/login_Window.py
@@ -1,20 +1,33 @@
-#Francisco de Jesus Melendez Simplina
-
 import sys
-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QMessageBox
-
-from Interfaces import vw_Login
-
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QWidget
+from Interfaces import vw_Login, vw_Registrar
+from Interfaces.vw_Registrar import Ui_Registrar
+from registrar_Window import registrar_Window
 
 class login_Window(QMainWindow, vw_Login.Ui_Login):
-
     def __init__(self, parent=None):
         super(login_Window, self).__init__(parent)
         self.setupUi(self)
+        self.bt_ingresar.clicked.connect(self.abrirRegistrar)
+
+
+    def abrirRegistrar(self):
+        try:
+            self.close()
+            self.ventana2 = QMainWindow()
+            self.ventana_registrar = Ui_Registrar()
+            self.ventana_registrar.setupUi(self.ventana2)
+            self.ventana2.show()
+
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mw = login_Window()
     mw.show()
-    app.exec()
+    sys.exit(app.exec_())
---
+++ Proyecto/Interfaces/registrar_Window.py
@@ -0,0 +1,33 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from Interfaces import vw_Registrar
+from Interfaces.vw_Login import Ui_Login
+
+
+class registrar_Window(QMainWindow, vw_Registrar.Ui_Registrar):
+    def __init__(self):
+        super(registrar_Window, self).__init__()
+        self.setupUi(self)
+
+        self.bt_registrar.clicked.connect(self.abrirLogin)
+
+    def abrirLogin(self):
+        try:
+            self.close()
+            self.ventana = QMainWindow()
+            self.ventana_login = Ui_Login()
+            self.ventana_login.setupUi(self.ventana)
+            self.ventana.show()
+
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mw = registrar_Window()
+    mw.show()
+    sys.exit(app.exec_())
+
