acdb1c546d461a5bbeb75d963c787a83796ae304
EdCenten0 <cchavarriacenteno8@gmail.com>
2023-05-22 22:35:56 -0600

    Login con pequeños cambios de diseño

--- Proyecto/Interfaces/vw_login.py
+++ Proyecto/Interfaces/vw_login.py
@@ -10,16 +10,15 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from Recursos import Recursos
-
 
 class Ui_Login(object):
     def setupUi(self, Login):
         Login.setObjectName("Login")
-        Login.resize(800, 600)
-        Login.setMinimumSize(QtCore.QSize(800, 600))
-        Login.setMaximumSize(QtCore.QSize(800, 600))
+        Login.resize(1000, 800)
+        Login.setMinimumSize(QtCore.QSize(1000, 800))
+        Login.setMaximumSize(QtCore.QSize(1000, 800))
         self.centralwidget = QtWidgets.QWidget(Login)
+        self.centralwidget.setStyleSheet("font-family:\"Inter\", sans-serif;")
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -43,13 +42,13 @@ class Ui_Login(object):
         self.horizontalLayout_2.setSpacing(0)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.label_6 = QtWidgets.QLabel(self.frame_fondo)
-        self.label_6.setStyleSheet("border-image: url(:/Imagenes/Fondo MAPA.jpeg);")
+        self.label_6.setStyleSheet("border-image: url(../Recursos/Fondo MAPA.jpeg);")
         self.label_6.setText("")
         self.label_6.setObjectName("label_6")
         self.horizontalLayout_2.addWidget(self.label_6)
         self.verticalLayout.addWidget(self.frame_fondo)
         self.frame_registrar = QtWidgets.QFrame(self.Background_Login)
-        self.frame_registrar.setStyleSheet("")
+        self.frame_registrar.setStyleSheet("font-family:\"Inter\", sans-serif;")
         self.frame_registrar.setFrameShape(QtWidgets.QFrame.Panel)
         self.frame_registrar.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_registrar.setObjectName("frame_registrar")
@@ -64,26 +63,23 @@ class Ui_Login(object):
         self.frame_4.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_4.setLineWidth(1)
         self.frame_4.setObjectName("frame_4")
-        self.frame = QtWidgets.QFrame(self.frame_4)
-        self.frame.setGeometry(QtCore.QRect(200, 70, 411, 211))
-        self.frame.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
-        self.frame.setStyleSheet("")
-        self.frame.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.frame.setFrameShadow(QtWidgets.QFrame.Plain)
-        self.frame.setLineWidth(0)
-        self.frame.setMidLineWidth(0)
-        self.frame.setObjectName("frame")
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame)
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_2.setSpacing(0)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.label_4 = QtWidgets.QLabel(self.frame)
-        self.label_4.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
+        self.label_3 = QtWidgets.QLabel(self.frame_4)
+        self.label_3.setGeometry(QtCore.QRect(50, 50, 221, 16))
+        self.label_3.setStyleSheet("")
+        self.label_3.setObjectName("label_3")
+        self.label = QtWidgets.QLabel(self.frame_4)
+        self.label.setGeometry(QtCore.QRect(50, 10, 231, 41))
+        self.label.setStyleSheet("font-weight:500;\n"
+"font-size:35px;\n"
+"")
+        self.label.setObjectName("label")
+        self.label_4 = QtWidgets.QLabel(self.frame_4)
+        self.label_4.setGeometry(QtCore.QRect(90, 90, 87, 25))
+        self.label_4.setStyleSheet("font-weight:500;\n"
+"font-size:22px;")
         self.label_4.setObjectName("label_4")
-        self.verticalLayout_2.addWidget(self.label_4, 0, QtCore.Qt.AlignHCenter)
-        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_2.addItem(spacerItem)
-        self.line_Clave = QtWidgets.QLineEdit(self.frame)
+        self.line_Clave = QtWidgets.QLineEdit(self.frame_4)
+        self.line_Clave.setGeometry(QtCore.QRect(190, 80, 411, 40))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -91,56 +87,55 @@ class Ui_Login(object):
         self.line_Clave.setSizePolicy(sizePolicy)
         self.line_Clave.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.line_Clave.setStyleSheet("QLineEdit {\n"
-"    border: 3px solid qlineargradient(spread:reflect, x1:0, y1:0, x2:1, y2:0.0170455, stop:0.261364 rgba(246, 143, 137, 255), stop:1 rgba(128, 74, 70, 255));\n"
-"\n"
-"border-radius:    5px;\n"
+"    border: 3px solid rgba(246, 143, 137, 255);\n"
+"    border-radius:    10%;\n"
+"    text-align: left;\n"
+"    font-weight:100;\n"
 "}")
-        self.line_Clave.setAlignment(QtCore.Qt.AlignCenter)
+        self.line_Clave.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
         self.line_Clave.setCursorMoveStyle(QtCore.Qt.LogicalMoveStyle)
         self.line_Clave.setObjectName("line_Clave")
-        self.verticalLayout_2.addWidget(self.line_Clave)
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_2.addItem(spacerItem1)
-        self.label_2 = QtWidgets.QLabel(self.frame)
-        self.label_2.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
+        self.label_2 = QtWidgets.QLabel(self.frame_4)
+        self.label_2.setGeometry(QtCore.QRect(50, 130, 141, 61))
+        self.label_2.setStyleSheet("font-weight:500;\n"
+"font-size:22px;")
         self.label_2.setObjectName("label_2")
-        self.verticalLayout_2.addWidget(self.label_2, 0, QtCore.Qt.AlignHCenter)
-        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_2.addItem(spacerItem2)
-        self.line_Usuario = QtWidgets.QLineEdit(self.frame)
-        self.line_Usuario.setStyleSheet("\n"
-"QLineEdit {\n"
-"    border: 3px solid qlineargradient(spread:reflect, x1:0, y1:0, x2:1, y2:0.0170455, stop:0.261364 rgba(246, 143, 137, 255), stop:1 rgba(128, 74, 70, 255));\n"
-"\n"
-"border-radius:    5px;\n"
+        self.line_Usuario = QtWidgets.QLineEdit(self.frame_4)
+        self.line_Usuario.setGeometry(QtCore.QRect(190, 140, 411, 40))
+        self.line_Usuario.setStyleSheet("QLineEdit {\n"
+"    border: 3px solid rgba(246, 143, 137, 255);\n"
+"    border-radius:10%;\n"
+"    font-weight:100;\n"
 "}")
         self.line_Usuario.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.line_Usuario.setAlignment(QtCore.Qt.AlignCenter)
+        self.line_Usuario.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
         self.line_Usuario.setObjectName("line_Usuario")
-        self.verticalLayout_2.addWidget(self.line_Usuario)
-        spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_2.addItem(spacerItem3)
-        self.lb_crear_Usuario = QtWidgets.QLabel(self.frame)
+        self.lb_crear_Usuario = QtWidgets.QLabel(self.frame_4)
+        self.lb_crear_Usuario.setGeometry(QtCore.QRect(190, 250, 114, 21))
+        self.lb_crear_Usuario.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.lb_crear_Usuario.setStyleSheet("QLabel:hover {\n"
 "    color: blue;\n"
 "}")
         self.lb_crear_Usuario.setObjectName("lb_crear_Usuario")
-        self.verticalLayout_2.addWidget(self.lb_crear_Usuario, 0, QtCore.Qt.AlignHCenter)
-        spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_2.addItem(spacerItem4)
-        self.bt_ingresar = QtWidgets.QPushButton(self.frame)
-        self.bt_ingresar.setStyleSheet("background-color: qlineargradient(spread:reflect, x1:0, y1:0, x2:1, y2:0.0170455, stop:0.261364 rgba(246, 143, 137, 255), stop:1 rgba(128, 74, 70, 255));\n"
-"color: rgb(255, 255, 255);")
+        self.bt_ingresar = QtWidgets.QPushButton(self.frame_4)
+        self.bt_ingresar.setGeometry(QtCore.QRect(190, 200, 411, 40))
+        self.bt_ingresar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.bt_ingresar.setStyleSheet("QPushButton{\n"
+"        background-color: qlineargradient(spread:reflect, x1:0, y1:0, x2:1, y2:0.0170455, stop:0.261364 rgba(246, 143, 137,             255), stop:1 rgba(128, 74, 70, 255));\n"
+"        color: rgb(255, 255, 255);\n"
+"        border-radius:10%;\n"
+"\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"        background-color: qlineargradient(spread:reflect, x1:0, y1:0, x2:1, y2:0.0170455, stop:0.261364 rgba(200, 90, 90,             255), stop:1 rgba(155, 90, 100, 255));\n"
+"}\n"
+"")
         self.bt_ingresar.setObjectName("bt_ingresar")
-        self.verticalLayout_2.addWidget(self.bt_ingresar)
-        self.label_3 = QtWidgets.QLabel(self.frame_4)
-        self.label_3.setGeometry(QtCore.QRect(220, 40, 381, 16))
-        self.label_3.setStyleSheet("font: 12pt \"MS Shell Dlg 2\";")
-        self.label_3.setObjectName("label_3")
-        self.label = QtWidgets.QLabel(self.frame_4)
-        self.label.setGeometry(QtCore.QRect(320, 0, 181, 31))
-        self.label.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";")
-        self.label.setObjectName("label")
+        self.label_5 = QtWidgets.QLabel(self.frame_4)
+        self.label_5.setGeometry(QtCore.QRect(270, 50, 131, 16))
+        self.label_5.setStyleSheet("font-weight:500;")
+        self.label_5.setObjectName("label_5")
         self.horizontalLayout_3.addWidget(self.frame_4)
         self.verticalLayout.addWidget(self.frame_registrar)
         self.horizontalLayout.addWidget(self.Background_Login)
@@ -152,15 +147,16 @@ class Ui_Login(object):
     def retranslateUi(self, Login):
         _translate = QtCore.QCoreApplication.translate
         Login.setWindowTitle(_translate("Login", "Login"))
+        self.label_3.setText(_translate("Login", "Inicia sesión con tu usuario de "))
+        self.label.setText(_translate("Login", "Iniciar Sesión"))
         self.label_4.setText(_translate("Login", "Usuario:"))
         self.line_Clave.setPlaceholderText(_translate("Login", "Ingrese su nombre de usuario"))
         self.label_2.setText(_translate("Login", "Contraseña:"))
         self.line_Usuario.setPlaceholderText(_translate("Login", "Ingrese su contraseña"))
         self.lb_crear_Usuario.setText(_translate("Login", "Crear un cuenta"))
         self.bt_ingresar.setText(_translate("Login", "Ingresar"))
-        self.label_3.setText(_translate("Login", "Inicia sesión con tu usuario de MAPA Interface"))
-        self.label.setText(_translate("Login", "Iniciar Sesión"))
-        Recursos
+        self.label_5.setText(_translate("Login", "MAPA Interface"))
+import Recursos
 
 
 if __name__ == "__main__":
