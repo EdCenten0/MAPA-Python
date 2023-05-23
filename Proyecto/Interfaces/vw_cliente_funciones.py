e73e4c341cb7782528cdfb5f05102d2b96583ffa
Rene-Sandoval <132532375+Rene-Sandoval@users.noreply.github.com>
2023-05-23 08:15:42 -0600

    f

--- Proyecto/Interfaces/vw_cliente_funciones.py
+++ Proyecto/Interfaces/vw_cliente_funciones.py
@@ -1,4 +1,4 @@
-#Francisco de Jesús Meléndez Simplina
+
 
 
 import sys
@@ -10,49 +10,48 @@ from Proyecto.Entidades import clientes
 
 
 
-class proveedor_Window(QMainWindow, vw_proveedor.Ui_Proveedores):
+class cliente_Window(QMainWindow, vw_cliente.Ui_Cliente):
 
     def __init__(self, parent=None):
-        super(proveedor_Window, self).__init__(parent)
+        super(cliente_Window, self).__init__(parent)
         self.setupUi(self)
 
         #Tabla
-        self.llenarTablaProveedor(dt_proveedor.Dt_Proveedor.listarProveedor())
-        self.tb_Proveedor.itemSelectionChanged.connect(self.obtenerDatosTablaProveedor)
+        self.llenarTablaCliente(dt_cliente.Dt_cliente.listarCliente())
+        self.tb_cliente.itemSelectionChanged.connect(self.obtenerDatosTablaCliente)
 
         #Botones
-        self.bt_Guardar.clicked.connect(self.guardarProveedor)
-        self.bt_Vaciar.clicked.connect(self.limpiarCampos)
+        self.bt_Guardar_Cliente.clicked.connect(self.guardarCliente)
+        self.bt_Vaciar_Cliente.clicked.connect(self.limpiarCampos)
 
 
     def limpiarCampos(self):
-        self.line_Id.setText("")
-        self.line_Nombre.setText("")
-        self.line_Ruc.setText("")
-        self.line_Correo.setText("")
-        self.line_Catalogo.setText("")
-        self.line_Direccion.setText("")
-        self.line_Telefono.setText("")
+        self.line_Cliente_Nombre.setText("")
+        self.line_Cliente_Apellido.setText("")
+        self.line_Cliente_Cedula.setText("")
+        self.line_Cliente_Direccion.setText("")
+        self.line_Cliente_Correo.setText("")
+        self.line_Cliente_Telefono.setText("")
 
-    def obtenerDatosTablaProveedor(self):
+    def obtenerDatosTablaCliente(self):
         # Selecciona la fila de la tabla
-        filaSeleccionada = self.tb_Proveedor.currentRow()
+        filaSeleccionada = self.tb_Cliente.currentRow()
 
-        id = self.tb_Proveedor.item(filaSeleccionada, 0).text()
-        nombre = self.tb_Proveedor.item(filaSeleccionada, 1).text()
-        email = self.tb_Proveedor.item(filaSeleccionada, 2).text()
-        telefono = self.tb_Proveedor.item(filaSeleccionada, 3).text()
-        catalogo = self.tb_Proveedor.item(filaSeleccionada, 4).text()
-        ruc = self.tb_Proveedor.item(filaSeleccionada, 5).text()
-        direccion = self.tb_Proveedor.item(filaSeleccionada, 6).text()
 
-        self.line_Id.setText(id)
-        self.line_Nombre.setText(nombre)
-        self.line_Correo.setText(email)
-        self.line_Telefono.setText(telefono)
-        self.line_Catalogo.setText(catalogo)
-        self.line_Ruc.setText(ruc)
-        self.line_Direccion.setText(direccion)
+        nombre = self.tb_Cliente.item(filaSeleccionada, 1).text()
+        apellido = self.tb_Cliente.item(filaSeleccionada, 2).text()
+        cedula = self.tb_Cliente.item(filaSeleccionada, 3).text()
+        direccion = self.tb_Cliente.item(filaSeleccionada, 4).text()
+        correo = self.tb_Cliente.item(filaSeleccionada, 5).text()
+        telefono = self.tb_Cliente.item(filaSeleccionada, 6).text()
+
+
+        self.line_Cliente_Nombre.setText(nombre)
+        self.line_Cliente_Apellido.setText(apellido)
+        self.line_Cliente_Cedula.setText(cedula)
+        self.line_Cliente_Direccion.setText(direccion)
+        self.line_Cliente_Correo.setText(correo)
+        self.line_Cliente_Telefono.setText(telefono)
 
     def notifMensaje(self, indicador, resultado):
 
@@ -64,42 +63,41 @@ class proveedor_Window(QMainWindow, vw_proveedor.Ui_Proveedores):
 
 
 
-    def llenarTablaProveedor(self, datos):
+    def llenarTablaCliente(self, datos):
         print("\nDatos de la Tabla Proveedor")
         i = len(datos)
-        self.tb_Proveedor.setRowCount(i)
+        self.tb_Cliente.setRowCount(i)
         tablerow = 0
 
         for row in datos:
             print(row)
-            self.tb_Proveedor.setItem(tablerow, 0, QTableWidgetItem(str(row["id_proveedor"])))
-            self.tb_Proveedor.setItem(tablerow, 1, QTableWidgetItem((row["nombre"])))
-            self.tb_Proveedor.setItem(tablerow, 2, QTableWidgetItem(str(row["ruc"])))
-            self.tb_Proveedor.setItem(tablerow, 3, QTableWidgetItem((row["email"])))
-            self.tb_Proveedor.setItem(tablerow, 4, QTableWidgetItem(str(row["catalogo"])))
-            self.tb_Proveedor.setItem(tablerow, 5, QTableWidgetItem((row["telefono"])))
-            self.tb_Proveedor.setItem(tablerow, 6, QTableWidgetItem(str(row["direccion"])))
+            self.tb_Cliente.setItem(tablerow, 0, QTableWidgetItem(str(row["nombre"])))
+            self.tb_Cliente.setItem(tablerow, 1, QTableWidgetItem((row["apellido"])))
+            self.tb_Cliente.setItem(tablerow, 2, QTableWidgetItem(str(row["cedula"])))
+            self.tb_Cliente.setItem(tablerow, 3, QTableWidgetItem((row["direccion"])))
+            self.tb_Cliente.setItem(tablerow, 4, QTableWidgetItem(str(row["correo"])))
+            self.tb_Cliente.setItem(tablerow, 5, QTableWidgetItem((row["telefono"])))
             tablerow = tablerow + 1
 
-    def guardarProveedor(self):
+    def guardarCliente(self):
 
         try:
-            Proveedor.nombre = self.line_Nombre.text()
-            Proveedor.correo = self.line_Correo.text()
-            Proveedor.direccion = self.line_Direccion.toPlainText()
-            Proveedor.catalogo = self.line_Catalogo.toPlainText()
-            Proveedor.ruc = self.line_Ruc.text()
-            Proveedor.telefono = self.line_Telefono.text()
+            clientes.nombre = self.line_Nombre.text()
+            clientes.apellido = self.line_Correo.text()
+            clientes.cedula = self.line_Direccion.toPlainText()
+            clientes.direccion = self.line_Catalogo.toPlainText()
+            clientes.correo = self.line_Ruc.text()
+            clientes.telefono = self.line_Telefono.text()
 
 
-            if self.line_Id.text() == "" and not self.line_Nombre.text() == "" and not self.line_Correo.text() == "" and not self.line_Ruc.text() == "" and not self.line_Telefono.text() == "" and not self.line_Direccion.toPlainText() == "" and not self.line_Catalogo.toPlainText() == "":
-                indicador = dt_proveedor.Dt_Proveedor.guardarProveedor(Proveedor)  # Recoge los datos en los "Lines" de Qt Desinger para guardarlos en la base de datos
+            if self.line_Cliente_Nombre.text() == "" and not self.line_Cliente_Apellido.text() == "" and not self.line_Cliente_Cedula.text() == "" and not self.line_Cliente_Direccion.text() == "" and not self.line_Cliente_Correo.toPlainText() == "" and not self.line_Cliente_Telefono.toPlainText() == "":
+                indicador = dt_cliente.Dt_Cliente.guardarCliente(clientes)  # Recoge los datos en los "Lines" de Qt Desinger para guardarlos en la base de datos
 
                 self.notifMensaje(indicador, "Guardados")
 
                 self.limpiarCampos()
 
-                self.llenarTablaUsuario(dt_proveedor.Dt_Proveedor.listarProveedor())  # Se reinicia la tabla para poder recargar los datos guardados
+                self.llenarTablaUsuario(dt_cliente.Dt_cliente.listarCliente())  # Se reinicia la tabla para poder recargar los datos guardados
 
             else:
 
@@ -112,6 +110,6 @@ class proveedor_Window(QMainWindow, vw_proveedor.Ui_Proveedores):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mw = proveedor_Window()
+    mw = cliente_Window()
     mw.show()
     app.exec()
