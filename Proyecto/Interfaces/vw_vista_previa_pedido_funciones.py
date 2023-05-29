308a3a8e469a9dc0532029b468aa2eff798717fe
EdCenten0 <cchavarriacenteno8@gmail.com>
2023-05-29 00:34:57 -0600

    Funcion para rellenar el combobox de vista_previa _pedidos

--- Proyecto/Interfaces/vw_vista_previa_pedido_funciones.py
+++ Proyecto/Interfaces/vw_vista_previa_pedido_funciones.py
@@ -1,9 +1,31 @@
+import sys
 import PyQt5
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QAbstractItemView
-from Interfaces import  vw_vista_previa_pedido
+from Datos import dt_Pedidos
+from Datos.dt_Pedidos import Dt_Pedidos
+from Interfaces import vw_vista_previa_pedido
 
+# Carlos Eduardo Chavarria Centeno (EdCenten0)
+# Universidad Centroamericana
 class VwVistaPreviaPedidosFunciones(QtWidgets.QMainWindow, vw_vista_previa_pedido.Ui_MainWindow):
     def __init__(self, parent=None):
         super(VwVistaPreviaPedidosFunciones, self).__init__(parent)
         self.setupUi(self)
+
+
+        # Llenado de combobox
+        self.llenarComboSeleccion(Dt_Pedidos.listarPedidos())
+
+    def llenarComboSeleccion(self, datos):
+        self.comboBox_2.clear()
+        for registro in datos:
+            self.comboBox_2.addItem(registro["descripcion"], registro["id_pedido"])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mw = VwVistaPreviaPedidosFunciones()
+    mw.show()
+    app.exec()
+
