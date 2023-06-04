1a783d08589cab1fdaac10a57d46815ec171a4e3
EdCenten0 <cchavarriacenteno8@gmail.com>
2023-06-03 22:10:59 -0600

    Se llenan los comboboxes y la tabla

--- Proyecto/Interfaces/vw_materiales_por_proveedor_funciones.py
+++ Proyecto/Interfaces/vw_materiales_por_proveedor_funciones.py
@@ -2,9 +2,11 @@ import sys
 
 import pymysql
 from PyQt5.QtWidgets import QMainWindow
+
+from Datos.dt_materiales import Dt_materiales
 from Interfaces import vw_materiales_por_proveedor
 from Entidades import materiales_por_proveedor
-from Datos import dt_materiales_por_proveedor, dt_proveedor
+from Datos import dt_materiales_por_proveedor, dt_proveedor, dt_materiales
 import PyQt5
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QAbstractItemView
@@ -19,65 +21,53 @@ class VwMaterialesPorProveedorFunciones(QtWidgets.QMainWindow, vw_materiales_por
     def __init__(self, parent=None):
         super(VwMaterialesPorProveedorFunciones, self).__init__(parent)
         self.setupUi(self)
+        self.listarMaterialesPorProveedor()
         self.llenarComboProveedor()
-        self.cb_proveedor.activated.connect(self.handleActivated)   
+        self.llenarComboMaterial()
 
 
-    # def llenarComboMaterial(self):
-    #     self.cb_material.clear()
-    #     try:
-    #
-    #     except pymysql.Error as e:
-    #         print(f"No se puedieron recuperar los datos para el combobox de materiales en el archivo"
-    #               f"de funciones de materiales_por_proveedor")
-    #
-    #     try:
-    #         for m in materiales_por_proveedor:
-    #             self.cb_material.addItem()
-    #     except Exception as e:
-    #         print(f"Ha ocurrido un error al rellenar el combobox de materiales en "
-    #               f"el archivo de funciones de materiales_por_proveedor : {e}")
 
+
+    def llenarComboMaterial(self):
+        self.cb_material.clear()
+
+        materiales = dt_materiales.Dt_materiales.listarMateriales()
+
+        try:
+            for m in materiales:
+                self.cb_material.addItem(m.nombre_material, m.id_material)
+        except Exception as e:
+            print(e)
+
+        n = self.cb_proveedor.currentData()
+        print(n)
 
     def llenarComboProveedor(self):
         self.cb_proveedor.clear()
 
         proveedores = dt_proveedor.Dt_Proveedor.listarProveedor()
-        print(f"No se puedieron recuperar los datos para el combobox de materiales en el archivo"
-            f"de funciones de materiales_por_proveedor")
+        print(proveedores)
 
         try:
             for p in proveedores:
-                self.cb_proveedor.addItem(p.index(), p.id_proveedor)
+                self.cb_proveedor.addItem(p["nombre"], p["id_proveedor"])
         except Exception as e:
-            print(f"Ha ocurrido un error al rellenar el combobox de materiales en "
+            print(f"Ha ocurrido un error al rellenar el combobox de proveedores en "
             f"el archivo de funciones de materiales_por_proveedor : {e}")
 
-    def handleActivated(self, index):
-        print(self.cb_proveedor.itemText(index))
-        print(self.cb_proveedor.itemData(index))
     def listarMaterialesPorProveedor(self):
-        print()
-        # materiales_por_proveedor = dt_materiales_por_proveedor.DtMaterialesPorProveedor.listar_materiales_por_proveedor()
-        # indexes = len(materiales_por_proveedor)
-        # self.table_materiales_requeridos_por_pedido.setRowCount(indexes)
-        # tablerow = 0
-        # for row in materiales_por_proveedor:
-        #     self.table_materiales_requeridos_por_pedido.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(str(row.id_materiales_por_proveedor)))
-        #     self.table_materiales_requeridos_por_pedido.setItem((tablerow, 1, QtWidgets.QTableWidgetItem(str(row))))
+        datos = dt_materiales_por_proveedor.DtMaterialesPorProveedor.listar_materiales_por_proveedor()
+        i = len(datos)
+        self.table_materiales_por_proveedor.setRowCount(i)
+        tablerow = 0
 
-    # def listarUsuarios(self):
-    #     usuarios = DT_Usuario.listarUsuario()
-    #     indexes = len(usuarios)
-    #     self.tw_usuarios.setRowCount(indexes)
-    #     tablerow = 0
-    #     for row in usuarios:
-    #         self.tw_usuarios.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(str(row.idusuario)))
-    #         self.tw_usuarios.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row.nombre))
-    #         self.tw_usuarios.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row.apellido))
-    #         self.tw_usuarios.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row.nombreusuario))
-    #         self.tw_usuarios.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(str(row.fecha_creacion)))
-    #         tablerow += 1
+        for row in datos:
+
+            self.table_materiales_por_proveedor.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(str(row.id_materiales_por_proveedor)))
+            self.table_materiales_por_proveedor.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(str(row.id_material)))
+            self.table_materiales_por_proveedor.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(str(row.id_proveedor)))
+            tablerow += 1
+
 
     def guardarMaterialesPorProveedorDesdeTf(self, index):
         mpp = materiales_por_proveedor.MaterialesPorProveedor()
@@ -98,7 +88,6 @@ class VwMaterialesPorProveedorFunciones(QtWidgets.QMainWindow, vw_materiales_por
 
 if __name__ == '__main__':
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = vw_materiales_por_proveedor.Ui_background()
