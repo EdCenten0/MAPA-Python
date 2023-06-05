f19eccf0297fd6bc7800aa55fcefc12c17fb3958
EdCenten0 <cchavarriacenteno8@gmail.com>
2023-06-05 00:28:08 -0600

    Update vw_ventana_principal_funciones.py

--- Proyecto/Interfaces/vw_ventana_principal_funciones.py
+++ Proyecto/Interfaces/vw_ventana_principal_funciones.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QMainWindow
 from vw_ventana_principal import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Interfaces import vw_ventana_principal, vw_cliente_funciones, vw_proveedor_funciones, \
-    vw_materiales_por_proveedor_funciones, vw_taller_funciones
+    vw_materiales_por_proveedor_funciones, vw_taller_funciones, vw_factura_funciones
 from Interfaces import vw_vista_previa_pedido_funciones
 
 # Carlos Eduardo Chavarria Centeno (EdCenten0)
@@ -22,7 +22,7 @@ class VentanaPrincipal(QtWidgets.QMainWindow, vw_ventana_principal.Ui_MainWindow
         self.bt_vista_previa_pedidos.clicked.connect(lambda: self.mostrar_formularios(vw_vista_previa_pedido_funciones.VwVistaPreviaPedidosFunciones()))
         self.bt_clientes.clicked.connect(lambda: self.mostrar_formularios(vw_cliente_funciones.Cliente_Window()))
         self.bt_proveedores.clicked.connect(lambda: self.mostrar_formularios(vw_proveedor_funciones.proveedor_Window()))
-
+        self.bt_facturas.clicked.connect(lambda: self.mostrar_formularios(vw_factura_funciones.Ui_MainWindow()))
         self.bt_materiales_por_proveedor.clicked.connect(lambda: self.mostrar_formularios(vw_materiales_por_proveedor_funciones.VwMaterialesPorProveedorFunciones()))
         self.bt_taller.clicked.connect(lambda: self.mostrar_formularios((vw_taller_funciones.vw_taller_funciones())))
     # Para este metodo es obliga    torio pasar la clase del formulario, por
