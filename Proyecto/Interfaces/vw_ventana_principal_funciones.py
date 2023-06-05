4db959102b4b0d7173d84fbc6d1760c822716a35
EdCenten0 <cchavarriacenteno8@gmail.com>
2023-06-05 10:12:00 -0600

    Se agrego la seguridad

--- Proyecto/Interfaces/vw_ventana_principal_funciones.py
+++ Proyecto/Interfaces/vw_ventana_principal_funciones.py
@@ -7,7 +7,8 @@ from Interfaces.vw_materiales_funciones import vw_materiales_funciones
 from vw_ventana_principal import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Interfaces import vw_ventana_principal, vw_cliente_funciones, vw_proveedor_funciones, \
-    vw_materiales_por_proveedor_funciones, vw_taller_funciones, vw_factura_funciones, vw_materiales
+    vw_materiales_por_proveedor_funciones, vw_taller_funciones, vw_factura_funciones, vw_materiales, \
+    vw_seguridad_funciones
 from Interfaces import vw_vista_previa_pedido_funciones
 
 # Carlos Eduardo Chavarria Centeno (EdCenten0)
@@ -27,6 +28,7 @@ class VentanaPrincipal(QtWidgets.QMainWindow, vw_ventana_principal.Ui_MainWindow
         self.bt_facturas.clicked.connect(lambda: self.mostrar_formularios(vw_factura_funciones.Ui_MainWindow()))
         self.bt_materiales_por_proveedor.clicked.connect(lambda: self.mostrar_formularios(vw_materiales_por_proveedor_funciones.VwMaterialesPorProveedorFunciones()))
         self.bt_taller.clicked.connect(lambda: self.mostrar_formularios((vw_taller_funciones.vw_taller_funciones())))
+        self.bt_seguridad.clicked.connect(lambda: self.mostrar_formularios(vw_seguridad_funciones.seguridad_Window()))
     # Para este metodo es obliga    torio pasar la clase del formulario, por
     # lo que se tiene que hacer referencia hasta llegar a la clase
     # por ejemplo archivo.clase o import la clase desde antes
